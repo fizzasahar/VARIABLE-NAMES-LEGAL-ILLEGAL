5f2f52b110012f8987d2a5938b4713ff5c944231
fizzasahar <fizzasahar44>
2024-05-10 13:12:23 +0500

    legal and illegal variables

---
+++ app.js
@@ -0,0 +1,11 @@
+var variable1, variable2, variable3;
+var myVariable;
+var firstName;
+var _lastName;
+var camelCaseVariable;
+var $price;
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain letters, digits, underscores, and dollar signs. For example $my_1stVariable<br>");
+document.write("Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name<br>");
+document.write("Variable names are case-sensitive<br>");
+document.write("Variable names should not be JavaScript reserved keywords<br>");
